83ad1ce78edbbe5b395beea395c94af478293641
Bandhan singh <parbhatbandhan@gmail.com>
2021-08-12 20:43:14 +0530

    navigation and fragments created

--- app/src/main/java/com/example/student_listing_app/data/StudentViewModel.kt
+++ app/src/main/java/com/example/student_listing_app/data/StudentViewModel.kt
@@ -1,12 +1,9 @@
-package com.example.student_listing_app
+package com.example.student_listing_app.data
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
-import com.example.student_listing_app.data.Student
-import com.example.student_listing_app.data.StudentDatabase
-import com.example.student_listing_app.data.StudentRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
